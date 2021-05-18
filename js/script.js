652d3cb3ac705b9cdbee219d622eb0313d6efb8f
Akash Ambashankar <akashambashankar@gmail.com>
2021-05-18 13:00:21 +0530

    changed nav to come fromm above

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function openMenu() {
-    document.querySelector('.nav-small').style.transform = "translateX(0%)";
+    document.querySelector('.nav-small').style.transform = "translateY(0)";
 }
 
 function closeMenu() {
-    document.querySelector('.nav-small').style.transform = "translateX(100%)";
+    document.querySelector('.nav-small').style.transform = "translateY(-48vh)";
 }
